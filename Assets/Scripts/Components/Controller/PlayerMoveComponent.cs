f3ab4e2ebc53a17eff6a110211ebd4701c4ef8e2
Golinsky <golinsky97@gmail.com>
2023-08-21 15:10:08 +0300

    - refactor

--- Assets/Scripts/Components/Controller/PlayerMoveComponent.cs
+++ Assets/Scripts/Components/Controller/PlayerMoveComponent.cs
@@ -6,16 +6,17 @@ using Component = WorkShop.LightWeightFramework.Components.Component;
 
 namespace WorkShop.Components.Controller
 {
-    public class PlayerMoveComponent:Component
+    public class PlayerMoveComponent : Component
     {
         private const float ZeroSpeed = 0.0f;
         private const float DefaultMagnitude = 1.0f;
-        
+        private const float TerminalVelocity = 53.0f; //what a magic number from unity default assets
+        private const float SpeedOffset = 0.1f;
+
         private readonly ITransformModel model;
         private readonly IMoveComponent moveComponent;
         private readonly IInputModelObserver inputModel;
 
-        private float terminalVelocity = 53.0f;//what a magic number from unity default assets
         private float verticalVelocity;
         private float jumpTimeoutDelta;
         private float targetRotation;
@@ -26,37 +27,31 @@ namespace WorkShop.Components.Controller
             this.moveComponent = moveComponent;
             this.inputModel = inputModel;
         }
-        
+
         protected override void OnInit(IGameObserver gameObserver)
         {
             jumpTimeoutDelta = model.JumpTimeout;
-
         }
 
         protected override void OnRelease()
         {
-            
         }
 
         public void Update(float deltaTime)
         {
             if (model.Grounded)
             {
-                // stop our velocity dropping infinitely when grounded
                 if (verticalVelocity < 0.0f)
                 {
                     verticalVelocity = -2f;
                 }
 
-                // Jump
                 if (inputModel.Jump && jumpTimeoutDelta <= 0.0f)
                 {
-                    // the square root of H * -2 * G = how much velocity needed to reach desired height
                     verticalVelocity = Mathf.Sqrt(model.JumpHeight * -2f * model.Gravity);
                     model.InvokeJumpEvent();
                 }
 
-                // jump timeout
                 if (jumpTimeoutDelta >= 0.0f)
                 {
                     jumpTimeoutDelta -= deltaTime;
@@ -65,17 +60,16 @@ namespace WorkShop.Components.Controller
             else
             {
                 jumpTimeoutDelta = model.JumpTimeout;
-                inputModel.Jump = false;//move this reset out of here
-
+                inputModel.Jump = false; //move this reset out of here
             }
-            
-            if (verticalVelocity < terminalVelocity)
+
+            if (verticalVelocity < TerminalVelocity)
             {
                 verticalVelocity += model.Gravity * deltaTime;
             }
 
             model.VerticalVelocity = verticalVelocity;
-            
+
             float targetSpeed = inputModel.Move == Vector2.zero
                 ? ZeroSpeed
                 : inputModel.Sprint
@@ -84,36 +78,28 @@ namespace WorkShop.Components.Controller
 
             float currentHorizontalSpeed = new Vector3(model.Velocity.x, 0.0f, model.Velocity.z).magnitude;
 
-            float speedOffset = 0.1f;
-            float inputMagnitude = inputModel.AnalogMovement ? inputModel.Move.magnitude : 1f;
+            float inputMagnitude = inputModel.AnalogMovement
+                ? inputModel.Move.magnitude
+                : DefaultMagnitude;
 
-            // accelerate or decelerate to target speed
-            if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-                currentHorizontalSpeed > targetSpeed + speedOffset)
+            if (currentHorizontalSpeed < targetSpeed - SpeedOffset ||
+                currentHorizontalSpeed > targetSpeed + SpeedOffset)
             {
-                // creates curved result rather than a linear one giving a more organic speed change
-                // note T in Lerp is clamped, so we don't need to clamp our speed
                 model.Speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
-                    Time.deltaTime * model.SpeedChangeRate);
+                    deltaTime * model.SpeedChangeRate);
 
-                // round speed to 3 decimal places
                 model.Speed = Mathf.Round(model.Speed * 1000f) / 1000f;
             }
             else
             {
                 model.Speed = targetSpeed;
             }
-            
-           
-            model.InputMagnitude = inputModel.AnalogMovement 
-                ? inputModel.Move.magnitude 
+            model.InputMagnitude = inputModel.AnalogMovement
+                ? inputModel.Move.magnitude
                 : DefaultMagnitude;
-
             model.MoveDirection = inputModel.Move;
-            Debug.Log(model.ToString());
-          //  Vector3 inputDirection = new Vector3(model.MoveDirection.x, 0.0f, model.MoveDirection.y).normalized;
 
-          moveComponent.Move(deltaTime, model.MoveDirection);
+            moveComponent.Move(deltaTime, model.MoveDirection);
         }
     }
 }
--- Assets/Scripts/ViewComponents/Movement/CharacterMovementViewComponent.cs
+++ Assets/Scripts/ViewComponents/Movement/CharacterMovementViewComponent.cs
@@ -1,46 +1,28 @@
-using StarterAssets;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace WorkShop.ViewComponents.Movement
 {
     public class CharacterMovementViewComponent : BaseMovementViewComponent<CharacterController>
     {
-     
-        
         private GameObject MainCamera => Camera.main.gameObject;
 
         // player
-        private float animationBlend;
         private float targetRotation = 0.0f;
         private float rotationVelocity;
-     //   private float speed;
 
         public override bool IsGrounded =>
             target.isGrounded; // Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers,
-
-        public override Vector3 Velocity => target.velocity;
         //  QueryTriggerInteraction.Ignore);
 
+        public override Vector3 Velocity => target.velocity;
 
-        protected override void OnInit()
-        {
-            base.OnInit();
-            // reset our timeouts on start
-            // if (_mainCamera == null)
-            // {
-            //     _mainCamera = FindObjectOfType<Camera>().gameObject;// GameObject.FindGameObjectWithTag("MainCamera");
-            // }
-        }
+        
 
         protected override void ChangePosition(Vector3 position)
         {
-
-            // normalise input direction
             Vector3 inputDirection = new Vector3(Model.MoveDirection.x, 0.0f, Model.MoveDirection.y).normalized;
 
-            // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-            // if there is a move input rotate player when the player is moving
+        
             if (Model.MoveDirection != Vector2.zero)
             {
                 targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
@@ -48,18 +30,15 @@ namespace WorkShop.ViewComponents.Movement
                 float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity,
                     Model.RotationSmoothTime);
 
-                // rotate to face input direction relative to camera position
                 transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
             }
 
 
             Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
 
-            // move the player
             target.Move(targetDirection.normalized * (Model.Speed * Time.deltaTime) +
                         new Vector3(0.0f, Model.VerticalVelocity, 0.0f) * Time.deltaTime);
 
-            // target.Move(Model.Direction * Model.Speed * Time.deltaTime);
         }
     }
 }
